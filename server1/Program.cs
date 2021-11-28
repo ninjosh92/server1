14895100503f25aa8cd68fa24233de45a16fd562
ninjosh92 <rafmendez92@yahoo.com>
2021-11-27 18:39:41 -0800

    Working_version
    This version runs the listener asynchronously and performs a task

--- server1/Program.cs
+++ server1/Program.cs
@@ -10,26 +10,40 @@ namespace sever1
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
             //Listens for a connection from any IP
             //TcpListener listener = new TcpListener(System.Net.IPAddress.Any, 49152);
             //IPAddress ipAddress = IPAddress.Parse("192.168.254.37");
             TcpListener listener = new TcpListener(System.Net.IPAddress.Any, 49152);
 
-            listenToClient(listener);
+
 
             
+            Task z = listenToClient(listener);
+
+            //int i = 10;
+
+            for (int i = 0; i < 100; i++)
+            {
+                
+                Console.WriteLine("looping {0} times", i);
+                System.Threading.Thread.Sleep(1000);
+            }
+            
+
         }
 
-        private static void listenToClient(TcpListener listener)
+        private static async Task listenToClient(TcpListener listener)
         {
             listener.Start();
             while (true)
             {
                 Console.WriteLine("Waiting for a connection.");
-                TcpClient client = listener.AcceptTcpClient();
+                //TcpClient client = listener.AcceptTcpClient();
+                TcpClient client = await listener.AcceptTcpClientAsync();
                 Console.WriteLine("Client accepted.");
+                
                 NetworkStream stream = client.GetStream();
                 StreamReader sr = new StreamReader(client.GetStream());
                 StreamWriter sw = new StreamWriter(client.GetStream());
@@ -46,6 +60,7 @@ namespace sever1
                         }
                     }
                     string request = Encoding.UTF8.GetString(buffer, 0, recv);
+                    
                     Console.WriteLine("request received: " + request);
                     sw.WriteLine("You rock!");
                     sw.Flush();
@@ -55,7 +70,9 @@ namespace sever1
                     Console.WriteLine("Something went wrong.");
                     sw.WriteLine(e.ToString());
                 }
+                
             }
+            
         }
     }
 }
